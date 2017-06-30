f1111dd8da6fe1a8a2be76909de718511ca6aa02
Oliver Blum <oblum@compvisgpu02.hci.uni-heidelberg.de>
2017-06-30 08:58:10 +0200

    init gen / disc model

--- ls_gan/models/disc.py
+++ ls_gan/models/disc.py
@@ -1,4 +1,42 @@
 
-def disc():
-    pass
+def one_class_disc(n_downsampling_layers = 3,
+                   filter_multiplier = 64):
+    """
+    Discriminator for GAN model
+    
+    Parameters
+    ----------
+    
+    n_downsampling_layers : int
+        number of downsampling layers. The input size can be computed by
+        4 * 2 ** (n_downsampling_layers)
+        
+    filter_multiplier : int
+        number which determines model complexity
+    """
+    
+    cnn = Sequential()
 
+    # first downsampling layer
+    ipt_size = 4 * 2 ** (n_downsampling_layers)
+    cnn.add(Convolution2D(filter_multiplier, (3, 3), padding='same', strides=(2, 2),
+                          input_shape=(ipt_size, ipt_size, 3)))
+    cnn.add(LeakyReLU())
+    cnn.add(Dropout(0.3))
+
+    # downsampling layers
+    mult = 1
+    for us_layer in range(n_downsampling_layers - 1):
+        
+        mult = mult * 2
+        cnn.add(Convolution2D(filter_multiplier * mult, (3, 3), 
+                              padding='same', 
+                              strides=(2, 2)))
+        cnn.add(LeakyReLU())
+        cnn.add(Dropout(0.3))
+
+    # dense layers
+    cnn.add(Flatten())
+    fake = Dense(1, activation='sigmoid', name='generation')(features)
+
+    return Model(inputs=image, outputs=fake)
--- ls_gan/models/gen.py
+++ ls_gan/models/gen.py
@@ -1,4 +1,47 @@
 
-def gen():
-    pass
+def one_class_gen(latent_size = 256, 
+                  n_upsampling_layers = 3,
+                  filter_multiplier = 64):
+    """
+    Generator for GAN model
+    
+    Parameters
+    ----------
+    
+    n_upsampling_layers : int
+        number of upsampling layers. The output size can be computed by
+        4 * 2 ** (n_upsampling_layers)
+        
+    filter_multiplier : int
+        number which determines model complexity
+    """
+        
+    latent = Input(shape=(latent_size, ))
+    
+    cnn = Sequential()
+    
+    mult = 2 ** (n_upsampling_layers - 1)
+    cnn.add(Dense(4 * 4 * filter_multiplier * mult, 
+                  input_dim=latent_size, 
+                  activation='linear'))
+    cnn.add(Reshape((4, 4, filter_multiplier * mult)))
 
+    # upsampling layers
+    for us_layer in range(n_upsampling_layers - 1):
+        
+        mult = mult / 2
+        cnn.add(UpSampling2D(size=(2, 2)))
+        cnn.add(Convolution2D(filter_multiplier * mult, 
+                              (5, 5), 
+                              padding='same',
+                              activation='relu', 
+                              kernel_initializer='glorot_normal'))    
+
+    # last upsampling layer with 3 filters (for the three color channels)
+    cnn.add(UpSampling2D(size=(2, 2)))
+    cnn.add(Convolution2D(3, (5, 5), padding='same',
+                          activation='tanh', kernel_initializer='glorot_normal'))
+
+    fake_image = cnn(latent)
+    
+    return Model(inputs=latent, outputs=fake_image)
